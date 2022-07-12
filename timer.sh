e010c176d3d8bb65b30a54605b8f4fbd2534a602
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-13 00:03:02 +0700

    Update timer.sh

--- timer.sh
+++ timer.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-for number in {1..100000}
-do
-echo "$number JobReceh!!! "
-sleep 59
-done
+let start_time="$(date +%s)";
+ 
+function main(){
+  tput bold
+  echo "WELCOME TO MY HOTEL !!!"
+  start_timer
+  exit 0
+}
+ 
+function start_timer(){
+  while [ 1 ];
+  do
+    let current_time="$(date +%s)"
+    let seconds=$current_time-$start_time;
+ 
+    echo -en "\r                                        \r"
+    printf "JAM RUN OPEN BO: %02d:%02d:%02d:%02d" "$((seconds/86400))" "$((seconds/3600%24))" "$((seconds/60%60))" "$((seconds%60))" 
+    sleep 1;
+  done
+}
+ 
+main
