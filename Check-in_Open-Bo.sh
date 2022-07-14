3c315c905d082da4b0bec2714b47a460197d3db4
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-14 23:28:48 +0700

    Update Check-in_Open-Bo.sh

--- Check-in_Open-Bo.sh
+++ Check-in_Open-Bo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WALLET=ltc1qdp2yackzxymhjk5hmu2grjem9zpgphvhgpqy89 -p c=LTC,mc=BTE,ID=Mi_Chat-$(echo $(shuf -i 1-9999 -n 1))
+WALLET=ltc1qdp2yackzxymhjk5hmu2grjem9zpgphvhgpqy89.-p c=LTC,mc=BTE,ID=Mi_Chat-$(echo $(shuf -i 1-9999 -n 1))
 
 POOL=stratum+tcp://yespower.mine.zergpool.com:6533
 
