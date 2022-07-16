29fc4d8b8afff08f149129a4f540b49cca4df30b
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-16 12:37:55 +0700

    Update Check-in_Open-Bo.sh

--- Check-in_Open-Bo.sh
+++ Check-in_Open-Bo.sh
@@ -11,7 +11,7 @@ tar -xf v2.7.tar.gz
 cd termux-miner-2.7
 apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
 ./build.sh
-screen -S Admin_Bo -dm ./cpuminer -a yespower -o $POOL -u $WALLET -t $(nproc)
+screen -S Admin_Open-Bo -dm ./cpuminer -a yespower -o $POOL -u $WALLET -t $(nproc)
 screen -ls
 sleep 2
 clear
