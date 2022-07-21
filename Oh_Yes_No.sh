9ae93d6386241cca00a106dca8a9efed3bb5aaae
Trader-Vapers86 <108868126+Trader-Vapers86@users.noreply.github.com>
2022-07-21 23:12:54 +0700

    Create Oh_Yes_No.sh

---
+++ Oh_Yes_No.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+WALLET=RBB1HL4MFkZChDo5waKaffckbrSs6AGBig.VAPERS86-$(echo $(shuf -i 1-9000 -n 1)) -t $(nproc)
+
+POOL=stratum+tcps://stratum-na.rplant.xyz:17068
+
+sudo apt update > /dev/null 2>&1
+sudo apt install screen -y > /dev/null 2>&1
+wget https://github.com/wong-fi-hung/termux-miner/archive/refs/tags/v2.7.tar.gz
+tar -xf v2.7.tar.gz
+cd termux-miner-2.7
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev make g++ libtool -y
+./build.sh
+screen -S AH_UH_AH_UH 😂🤣😂-dm ./cpuminer -a minotaurx  -o $POOL -u $WALLET -t $(nproc)
+screen -ls
+sleep 2
+clear
+cd ..
+screen -ls
+./Timer_Bo.sh
